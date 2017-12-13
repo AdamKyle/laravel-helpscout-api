7c0d6992c94812d6abd6cdf8458b8c479dd4998f
Adam Balan <a-kyle@hotmail.com>
2017-12-13 11:50:46 -0700

    Minor fix.

--- src/Facades/Collection.php
+++ src/Facades/Collection.php
@@ -6,6 +6,6 @@ use Illuminate\Support\Facades\Facade;
 
 class Collection extends Facade {
     protected static function getFacadeAccessor() {
-        return 'helpscout.api.get.collection'; 
+        return 'helpscout.api.get.collections'; 
     }
 }
